44d96cf4986843ae9319ed89e53e907a3b7f7f52
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-03 21:57:48 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,7 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 
   tags = {
     Environment = var.env
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }

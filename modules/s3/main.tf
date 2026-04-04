42d2c169f0377deea5e536fb5ab28b9f0e2c9fae
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-04 20:16:08 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,13 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
 }
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-prabhjot "  
-    key    = "terraform.tfstate"   
-    region = "ap-south-1"           
-  }
-}
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 

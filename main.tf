c7d2b18f4e1ed44d58341ad546fb68fa91dd2b26
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-05 20:03:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-prabhjot "
+    bucket = "terraform-bucket-prabhjot"
     key    = "terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-west-2"
   }
 }

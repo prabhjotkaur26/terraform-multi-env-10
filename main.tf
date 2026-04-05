439268327f6220ae4975edb58f9050e77fc0aadd
Your Name <your-email@example.com>
2026-04-05 20:13:42 +0530

    my first commit

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-terraform
-backend "s3" {
-  bucket         = "terraform-bucket-prabhjot"
-  key            = "terraform.tfstate"
-  region         = "ap-south-1"
-  encrypt        = true
-  dynamodb_table = "terraform-locks"
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-prabhjot"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 }

462fc7b865d473996cc41b24025ed382bf779413
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-06 18:35:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,7 @@
-provider "aws" {
-  region = "ap-south-1"   # default
-}
-
-# Other regions
-provider "aws" {
-  alias  = "us"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "eu"
-  region = "eu-west-1"
-}
-
-# Bucket in ap-south-1
-resource "aws_s3_bucket" "india_bucket" {
-  bucket = "my-bucket-india-12345"
-}
-
-# Bucket in us-east-1
-resource "aws_s3_bucket" "us_bucket" {
-  provider = aws.us
-  bucket   = "my-bucket-us-12345"
-}
-
-# Bucket in eu-west-1
-resource "aws_s3_bucket" "eu_bucket" {
-  provider = aws.eu
-  bucket   = "my-bucket-eu-12345"
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-prabhjot"
+    key    = "terraform.tfstate"
+    region = "ap-south-1" 
+  }
 }

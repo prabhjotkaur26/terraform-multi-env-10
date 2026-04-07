978100700e14f8fb1c1fe9c017a38abebdd8bf50
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 07:20:57 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,14 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
 terraform {
   backend "s3" {
     bucket = "dev-backend-buckett "
     key    = "dev/terraform.tfstate"
     region = "ap-south-1"
   }
-}
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "dev-terraform-2026"
 }

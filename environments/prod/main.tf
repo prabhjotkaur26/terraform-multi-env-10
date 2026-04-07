51c52039c592167abee53b2871fbd06e19bcd613
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 17:00:02 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   region = var.aws_region
 }
-terraform {
-  backend "s3" {
-    bucket = "prod-backend-buckett"
-    key    = "prod/terraform.tfstate"
-    region = "eu-west-1"
-  }
+
+module "s3" {
+  source      = "../../modules/s3"
+  bucket_name = var.bucket_name
+  environment = var.environment
 }

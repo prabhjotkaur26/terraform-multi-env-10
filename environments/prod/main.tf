bb9968d6a4a9512bb97e4e66963b154e6030fb27
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 07:10:11 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,15 +1,11 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 terraform {
   backend "s3" {
     bucket = "prod-backend-buckett"
     key    = "prod/terraform.tfstate"
     region = "eu-west-1"
   }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "prod-terraform-2026"
 }

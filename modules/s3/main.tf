feb01fc9ccd544860e52240ea46ac5c740607c7e
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 19:51:57 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,11 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+}
 
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
-    ManagedBy   = "Terraform"
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }

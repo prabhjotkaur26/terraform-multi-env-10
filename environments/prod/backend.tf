3bdb578245e8c8cc3effa3a0d48f5c514cd90eb9
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 17:26:30 +0530

    Update backend.tf

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-bucket-prabhjot"
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock-123"
   }
 }

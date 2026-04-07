2245bd84b9c0254243784ecaeb1641a823bffbe9
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 17:15:19 +0530

    Update backend.tf

--- environments/qa/backend.tf
+++ environments/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-sachin"
+    bucket         = "terraform-bucket-prabhjot"
     key            = "qa/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"

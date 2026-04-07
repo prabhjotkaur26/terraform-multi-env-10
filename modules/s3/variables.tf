17a6d39c778207a0be5f341053c3c0e3f80598e7
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 18:14:33 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,3 +1,7 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
 variable "bucket_name" {
   description = "Final bucket name (auto-generated)"
   type        = string

ef1415bc2e8df3d23ace0d0b6faac82adedae91d
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-07 17:52:13 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,2 +1,9 @@
-variable "aws_region" {}
-variable "env" {}
+variable "bucket_name" {
+  description = "Final bucket name (auto-generated)"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}

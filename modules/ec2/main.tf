6997c8f1636089cc21d366751dc8c7573c2b3589
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-08 11:00:46 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = var.ami_map[var.aws_region]
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name        = "${var.environment}-ec2"

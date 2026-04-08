3bfda4c46c0784a9177cdd137764cbe1d064770e
prabhjotkaur26 <prabh008968@gmail.com>
2026-04-08 10:57:09 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,7 +1,11 @@
-variable "environment" {
-  type = string
-}
+variable "environment" {}
+variable "aws_region" {}
 
-variable "aws_region" {
-  type = string
+variable "ami_map" {
+  type = map(string)
+  default = {
+    ap-south-1 = "ami-0f58b397bc5c1f2e8"
+    us-east-1  = "ami-0c02fb55956c7d316"
+    eu-west-1  = "ami-0dad359ff462124ca"
+  }
 }
